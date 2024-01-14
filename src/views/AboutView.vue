756debb67ffdf95600c63293288880caad5fafac
Matteo Calderaro <calderaro.matteo@proton.me>
2024-01-14 01:01:05 +0100

    j1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+  let count = ref(0)
+  let styleButton = ref({
+    color: 'red',
+    fontSize: '20px'
+  })
+  let answer = ref(true)
+  let messages = ref([
+    { id: 1, text: 'Hello' },
+    { id: 2, text: 'World' },
+    { id: 3, text: '!' }
+  ])
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p v-if="answer">
+      {{ count }}
+    </p>
+    <p v-else>
+      {{ count + 5 }}
+    </p>
+    <button @click="answer = !answer" :style="styleButton">
+      Add
+    </button>
+    <p v-for="(msg, i) in messages" :key="msg.id">
+      {{ i }} - {{ msg.text }} ({{ msg.id }})
+    </p>
   </div>
 </template>
 
 <style>
+
+p {
+  font-size: large;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
@@ -12,4 +44,12 @@
     align-items: center;
   }
 }
+
+.about {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
 </style>
