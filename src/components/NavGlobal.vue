e80bcae786c7df70746dcfc574b877a32ba2c0fd
Matteo Calderaro <88966144+Totamaa@users.noreply.github.com>
2024-01-19 15:48:02 +0100

    Tuto (#5)
    * j1
    
    * temp
    
    ---------
    
    Co-authored-by: Matteo Calderaro <calderaro.matteo@proton.me>

---
+++ src/components/NavGlobal.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Accueil</router-link>
+            </li>
+            <li>
+                <router-link to="/about">A propos</router-link>
+            </li>
+            <li>
+                <router-link to="/contact">Contact</router-link>
+            </li>
+        </ul>
+    </nav>
+
+</template>
